b82fcaa3d53893004ce1cc997d1ff8d5e75fcefb
hallyssonmateus <hallysson_gato@hotmail.com>
2024-08-12 16:48:59 -0300

    Primeiro commit - Adicionando arquivos

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Home() {
+  return (
+      <div className='container'>
+      <form>
+          <h1>Cadastro de usuarios</h1>
+        </form>
+      </div>
+      
+  )
+}
+
+export default Home
