a855dc164c70ae61357007857626fba7f46c1704
hallyssonmateus <hallysson_gato@hotmail.com>
2024-08-30 03:07:14 -0300

    feat: Estilização do formularios e a caixa de dados dos usuarios

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,47 @@
 import './style.css'
 
 function Home() {
+
+  const users = [{
+    id: 'dmknsvjsjnvjnv',
+    name: 'Bruno',
+    age: 32,
+    email: 'brunodamente@gmail.com',
+  }, {
+    id: 'daahhahahah',
+    name: 'Elaine',
+    age: 23,
+    email: 'elainedamente@gmail.com',
+  }]
+
   return (
-      <div className='container'>
+    <div className='container'>
       <form>
-          <h1>Cadastro de usuarios</h1>
-        </form>
-      </div>
-      
+        <h1>Cadastro de usuarios</h1>
+        <input name='nome' type='text' placeholder='Digite seu nome'/>
+        <input name='idade' type='number' placeholder='Digite sua idade' />
+        <input name='email' type='email' placeholder='Digite seu email'/>
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span> </p>
+            <p>Email: <span>{user.email}</span> </p>
+          </div>
+          <div>
+            <button>
+              <a href='https://svgshare.com/s/16qg' ><img src='https://svgshare.com/i/16qg.svg' title='trash' /></a>
+            </button>
+          </div>
+        </div>
+      ))}
+
+
+    </div>
+
   )
 }
 
