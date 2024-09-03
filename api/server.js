0666a7f7e82004b54c07a5392905618e6ab2790b
hallyssonmateus <hallysson_gato@hotmail.com>
2024-09-03 17:00:46 -0300

    feat: Criação dos endpoints de CRUD ja com a interação com o banco de dados - testados e funcionando corretamente

--- api/server.js
+++ api/server.js
@@ -5,18 +5,66 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-const users = []
 
 //Criar usuarios
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+
     res.status(201).json(req.body)
 })
 
 
 //Listar os usuarios
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    let users = []
+
+    if (req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    } else {
+        users = await prisma.user.findMany()
+    }
+
     res.status(200).json(users)
+})
+
+//Editar Usuario - Route Params
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+//Deletar Usuarios
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: "Usuario deletado com sucesso!"})
 })
 
 //Indicação da porta de requisição da API
