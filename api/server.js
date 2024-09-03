76485d7875e65ad0212ffa7e5756aef0ff59b6f7
hallyssonmateus <hallysson_gato@hotmail.com>
2024-09-03 03:16:13 -0300

    feat: Adicão da pasta Api com dois endpoints Get e Post

---
+++ api/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+const users = []
+
+//Criar usuarios
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+    res.status(201).json(req.body)
+})
+
+
+//Listar os usuarios
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+})
+
+//Indicação da porta de requisição da API
+app.listen(3000)
